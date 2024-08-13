c90c5aedb9b2172a71323e852c0dd752239658bf
Pathiphat1922 <pathiphat228@gmail.com>
2024-08-13 16:57:46 +0700

    add img

--- webpage/urls.py
+++ webpage/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("contact/", views.contactUs, name="contact"),
     path('for_test/', views.forPage, name="for_test"),
     path("card_template/", views.cardPage, name="card_template"),
-    path("card_color/", views.cardColorUs, name="card_color"),
+    path("cardcolor/", views.cardColorUs, name="card_color"),
 ]
