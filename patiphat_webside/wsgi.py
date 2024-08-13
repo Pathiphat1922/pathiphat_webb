8d18b6306737747d7e8762ccc92900ebdf029692
Pathiphat1922 <pathiphat228@gmail.com>
2024-08-13 16:32:59 +0700

    first commit

--- patiphat_webside/wsgi.py
+++ patiphat_webside/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sirimongkol_web project.
+WSGI config for patiphat_webside project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sirimongkol_web.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'patiphat_webside.settings')
 
 application = get_wsgi_application()
 
--- webpage/views.py
+++ webpage/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def indexPage(request):
     return render(request, 'index.html')
+
 
 def aboutUs(request):
     return render(request, 'about.html')
@@ -10,6 +13,7 @@ def aboutUs(request):
 
 def contactUs(request):
     return render(request, 'contact.html')
+
 
 def forPage(request):
     context = {}
@@ -21,3 +25,14 @@ def forPage(request):
 
 def cardPage(request):
     return render(request, 'card_template.html', {'range': range(100)})
+
+
+def cardColorUs(request):
+    context = {
+        'color': 'all',
+    }
+
+    if request.method == "GET" and request.GET.get('color') != None:
+        context['color'] = request.GET.get('color')
+
+    return render(request, 'card_color.html', context)
